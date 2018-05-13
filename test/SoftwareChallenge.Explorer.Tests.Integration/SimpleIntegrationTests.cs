0104bc094153d460ddd04ff510a7c37f5a9d4e52
bornwarrior <j3patel@gmail.com>
2018-05-12 22:44:41 -0400

    Challanege 1 - Added Simple REST API (using .Net Core WebAPI template) and it test

---
+++ test/SoftwareChallenge.Explorer.Tests.Integration/SimpleIntegrationTests.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+
+
+
+namespace SoftwareChallenge.Explorer.Tests.Integration
+{
+    public class SimpleIntegrationTests
+    {
+        private readonly TestServer testServer;
+        private readonly HttpClient testClient;
+
+        public SimpleIntegrationTests()
+        {
+              testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+              testClient = testServer.CreateClient();
+        }
+
+        [Fact]
+        public async void ForAnIdReceiveHElloWorld()
+        {
+            //arrange
+            int id = 1;
+            string expectedResult = "Hello World";
+            
+            //act
+            var getResponse = await testClient.GetAsync($"api/values/{id}");
+
+            //asert
+            getResponse.EnsureSuccessStatusCode();
+            
+
+            string actualResult = await getResponse.Content.ReadAsStringAsync();
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+    }
+}
